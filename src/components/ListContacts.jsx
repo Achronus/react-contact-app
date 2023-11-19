39219ca3620b511a913668d92d24821bc7560fd3
Ryan Partridge <rpartridge101@yahoo.co.uk>
2023-11-19 21:42:32 +0000

    Initial commit

---
+++ src/components/ListContacts.jsx
@@ -0,0 +1,12 @@
+
+
+const ListContacts = ({ contacts }) => {
+    console.log(contacts);
+    return (
+        <ol className="contact-list">
+            
+        </ol>
+    );
+}
+ 
+export default ListContacts;
