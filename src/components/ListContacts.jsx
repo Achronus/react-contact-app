275d4655af135552ddb297b53450943f52633e55
Ryan Partridge <rpartridge101@yahoo.co.uk>
2023-11-23 04:36:58 +0000

    Added contact count on search

--- src/components/ListContacts.jsx
+++ src/components/ListContacts.jsx
@@ -9,6 +9,10 @@ const ListContacts = ({ contacts, onDeleteContact }) => {
         setQuery(query.trimStart());
     }
 
+    const clearQuery = () => {
+        updateQuery("");
+    }
+
     const showingContacts = query === "" ? contacts : contacts.filter((c) => c.name.toLowerCase().includes(query.toLowerCase()));
 
     return (
@@ -16,6 +20,17 @@ const ListContacts = ({ contacts, onDeleteContact }) => {
             <div className="list-contacts-top">
                 <input className="search-contacts" type="text" placeholder="Search Contacts" value={query} onChange={(event) => updateQuery(event.target.value)} />
             </div>
+
+            {
+                showingContacts.length !== contacts.length && (
+                    <div className="showing-contacts">
+                        <span>
+                            Now showing {showingContacts.length} of {contacts.length}
+                        </span>
+                        <button onClick={clearQuery}>Show all</button>
+                    </div>
+                )
+            }
 
             <ol className="contact-list">
                 {showingContacts.map((contact) => (
